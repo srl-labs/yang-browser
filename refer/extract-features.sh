521f1f2a4667407121f470d5a55422d87930a5f7
hellt <dodin.roman@gmail.com>
2023-04-14 22:45:58 +0300

    added background jobs

--- refer/extract-features.sh
+++ refer/extract-features.sh
@@ -8,8 +8,11 @@ rm -f 72*.json
 for node in "clab-all-7220-IXR-D1" "clab-all-7220-IXR-D2" "clab-all-7220-IXR-D2L" "clab-all-7220-IXR-D3" "clab-all-7220-IXR-D3L" "clab-all-7220-IXR-D5" "clab-all-7220-IXR-H2" "clab-all-7220-IXR-H3" "clab-all-7250-IXR-6" "clab-all-7250-IXR-6e" "clab-all-7250-IXR-10" "clab-all-7250-IXR-10e"
 do
     platf_name=${node#clab-all-}
-    docker exec ${node} sr_cli 'info from state system features | as json' | jq .system.features >> ${platf_name}.json
+    docker exec ${node} sr_cli 'info from state system features | as json' | jq .system.features >> ${platf_name}.json &
 done
+
+wait
+echo "Features have been extracted to JSON files, proceeding with aggregating them into a single file..."
 
 # file name to extract features for all platforms
 fn="features.txt"
