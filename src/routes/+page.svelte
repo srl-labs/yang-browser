9532c05af07e4a2ac1e53d92066e958be6319a98
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-01-05 11:29:31 +0100

    home alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,23 +48,27 @@
 
 <div class="flex flex-col items-center min-h-screen pt-5 has-header-img font-nunito">
   <div class="flex-grow-0 flex-shrink-0">
-    <div class="text-sm">
-      <a class="px-3 py-1.5 bg-blue-500 text-white rounded-full text-center inline-block items-center" href="https://learn.srlinux.dev/yang/browser" target="_blank" rel="noreferrer">
-        <span class="icon"><i class="fab fa-leanpub"></i></span>
-        <span>Learn SR Linux</span>
+    <div class="w-screen text-right text-xs mr-10">
+      <a class="px-3 py-1.5 border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white rounded-full text-center inline-flex items-center" href="https://learn.srlinux.dev/yang/browser" target="_blank" rel="noreferrer">
+        <svg class="w-3 h-3 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 16.5c0-1-8-2.7-9-2V1.8c1-1 9 .707 9 1.706M10 16.5V3.506M10 16.5c0-1 8-2.7 9-2V1.8c-1-1-9 .707-9 1.706"/>
+        </svg>
+        Learn SR Linux
       </a>
-      <a class="px-3 py-1.5 bg-blue-500 text-white rounded-full text-center inline-block items-center ml-1" href="https://github.com/nokia/srlinux-yang-models" target="_blank" rel="noreferrer">
-        <span class="icon"><i class="fab fa-github"></i></span>
-        <span>Yang models</span>
+      <a class="px-3 py-1.5 border border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white rounded-full text-center inline-flex items-center ml-1" href="https://github.com/nokia/srlinux-yang-models" target="_blank" rel="noreferrer">
+        <svg class="w-3 h-3 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+        </svg>
+        Yang models
       </a>
     </div>
   </div>
-  <div class="flex-grow-1 flex-shrink-0 m-auto px-10">
+  <div class="flex-grow-1 flex-shrink-0 m-auto px-6 py-10">
     <div class="md:flex gap-x-20 items-center justify-between">
       <div class="flex">
         <div class="py-5 space-y-6">
-          <p><img src="/images/nokia_b.png" width="90" alt="Logo" /></p>
-          <h3 class="text-3xl font-light text-blue-900">SR Linux Yang Models</h3>
+          <p><img src="/images/nokia_b.png" width="80" alt="Logo" /></p>
+          <h3 class="text-2xl font- text-blue-900">SR Linux Yang Models</h3>
           {#each top4 as major}
             <div class="flex items-center">
               <p class="mr-4 text-xl">{major}</p>
