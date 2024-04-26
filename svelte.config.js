0b89edcabff80225c5f07e24dbea94ec7cf64a95
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-04-26 22:06:16 +0200

    svelte update

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// See https://kit.svelte.dev/docs/adapter-cloudflare 
-		// for more information about cloudfare adapters
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-	})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
