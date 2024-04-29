00bcda500d9642e4d70d2020716ba0b98521c896
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-04-29 13:16:57 +0200

    refers and logo url path

---
+++ refer/generate.sh
@@ -0,0 +1,179 @@
+#!/usr/bin/bash
+
+# Script requires docker, gnmic (>v0.21.0), jstree-to-bulma.html, index.html.tmpl
+
+# In case of bad interpreter error perform:
+# sed -i -e 's/\r$//' generate.sh
+
+# execute from repo root as `bash refer/generate.sh <srl_nokia | openconfig> [<srl_release1> <srl_release2> ...]`
+# example: bash refer/generate.sh srl_nokia 22.11.1
+
+# convert features from the D2L json file to pyang format so that we can generate
+# tree and jstree output for a featureset of D2L
+extract_pyang_features() {
+  features_mod="srl_nokia-features"
+  # read the 7220-IXR-D2L.json file and remove features from it found in unused-features.json
+  # this is needed since the info command provides all features, even those that do not exist in yang features.
+  # the paths are relative to the ./refer/srl-$SRL_VER-yang-models folder
+  fs=$(jq --argjson unusedFeatures "$(cat ../unused-features.json)" 'map(select(. as $in | any($unusedFeatures[]; . == $in) | not))' ../../7220-IXR-D2L.json | jq -r 'join(",")')
+  echo ${features_mod}:${fs}
+}
+
+MODEL_TYPE="$1"
+
+# SR Linux release version are passed as a space separated list of release version
+SRL_VER_LIST="${@:2}"
+
+GNMIC_CONTAINER=ghcr.io/openconfig/gnmic:0.32.0
+
+PYANG_CONTAINER=ghcr.io/hellt/pyang:2.5.3
+
+# Verify if model type is provided
+if [ "$MODEL_TYPE" = "srl_nokia" ]; then
+  MODEL_PATH="srl_nokia/models"
+  GNMIC_ADDONS="--dir ietf/ --dir openconfig"
+elif [ "$MODEL_TYPE" = "openconfig" ]; then
+  MODEL_PATH="openconfig"
+  GNMIC_ADDONS="--dir ietf/ --dir iana/"
+else
+  echo "Error: Model must be specified (srl_nokia | openconfig)."
+  exit 1
+fi
+
+# change into the script directory
+SCRIPT_DIR="$(realpath $(dirname "$0"))"
+cd $SCRIPT_DIR
+
+# PYANG RESPONSE HANDLER
+response_handler() {
+  if [[ $1 == *"not found"* ]]; then
+    MISSING=$(echo $1 | grep -oP '\S+ is not found' | awk -F '"' '{print $2}' | sort | uniq | paste -s -d, -)
+    echo "$2: Model [$MISSING] not found."
+  else
+    echo "$2: OK"
+  fi
+}
+
+
+
+for SRL_VER in ${SRL_VER_LIST[@]}; do
+  echo
+  # Extract end user release cycle
+  SRL_VER_CYCLE=v$(echo $SRL_VER | cut -d'-' -f1)
+
+  # PULL SRL YANG MODEL
+  YANG_DIR_NAME="$(pwd)/srl-$SRL_VER-yang-models"
+
+  docker pull ghcr.io/nokia/srlinux:$SRL_VER || echo "using local image"
+  id=$(docker create ghcr.io/nokia/srlinux:$SRL_VER foo)
+
+  mkdir -p $YANG_DIR_NAME
+  docker cp $id:/opt/srlinux/models/. $YANG_DIR_NAME
+  docker rm $id
+
+  cd $YANG_DIR_NAME
+  PROCEED=0
+  if [ "$MODEL_TYPE" = "srl_nokia" ]; then
+    mkdir -p ../../release/$SRL_VER_CYCLE
+    OUT_DIR=$(realpath ../../release/$SRL_VER_CYCLE)
+    PROCEED=1
+  elif [ "$MODEL_TYPE" = "openconfig" ]; then
+    if [ -d "./openconfig" ]; then
+      mkdir -p ../../release/$SRL_VER_CYCLE/openconfig
+      OUT_DIR=$(realpath ../../release/$SRL_VER_CYCLE/openconfig)
+      PROCEED=1
+    else
+      echo
+      echo "Warning: $SRL_VER does not have any openconfig yang model."
+    fi
+  fi
+
+  if [ $PROCEED -eq 1 ]; then
+    # if [ "$1" = "srl_nokia" ]; then
+      # placeholder for models massaging if needed for a particual model/release
+      # sed -i 's/modifier "invert-match";//g' srl_nokia/models/common/srl_nokia-common.yang # remove modifiers that are not supported by goyang
+      # echo ""
+    # fi
+
+    echo
+    # PYANG TREE + JSTREE
+    #---------------------
+    # Create combined folder with all YANG files
+    mkdir -p combined
+    find ./$MODEL_PATH -name "*.yang" | xargs -I % cp % combined
+
+    # extract pyang features
+    features=$(extract_pyang_features)
+    # echo "Features: $features"
+    
+    # PYANG TREE
+    # this is just for testing of features that are missing in yang
+    # comment out when done
+    # docker run --rm -v $(pwd):/yang $PYANG_CONTAINER pyang -p ietf:iana:$MODEL_PATH -f tree -F ${features} combined/*.yang -o tree.txt
+    # exit 1
+    # end of testing
+
+    RESPONSE=$(docker run --rm -v $(pwd):/yang $PYANG_CONTAINER pyang -p ietf:iana:$MODEL_PATH -f tree -F ${features} combined/*.yang -o tree.txt 2>&1 >/dev/null)
+    response_handler "$RESPONSE" "$SRL_VER-tree"
+
+    # PYANG JSTREE
+    RESPONSE=$(docker run --rm -v $(pwd):/yang $PYANG_CONTAINER pyang -p ietf:iana:$MODEL_PATH --plugindir /opt/pyang-oc-plugin -f oc-jstree -F ${features} --oc-jstree-strip combined/*.yang -o tree.html 2>&1 >/dev/null)
+    response_handler "$RESPONSE" "$SRL_VER-jstree"
+
+    # Delete combined folder
+    rm -rf combined
+
+    # JSTREE PRETTY
+    #-------------------
+    # Navigate into release folder
+    mv tree.txt tree.html $OUT_DIR
+    cd $OUT_DIR
+
+    # Add custom Bulma CSS break point (REMOVE)
+    sed -i '0,/^<tr id="/s/^<tr id="/REMOVE\n<tr id="/' tree.html
+
+    # Delete all lines from start of the file till the line REMOVE
+    sed -i '0,/^REMOVE$/d' tree.html
+
+    # Append Bulma CSS
+    if [ "$MODEL_TYPE" = "srl_nokia" ]; then
+      cat $SCRIPT_DIR/jstree-to-bulma.html tree.html >tmp.html
+    elif [ "$MODEL_TYPE" = "openconfig" ]; then
+      cat $SCRIPT_DIR/oc-jstree-to-bulma.html tree.html >tmp.html
+    fi
+    cat $SCRIPT_DIR/jstree-to-bulma.html tree.html >tmp.html
+    mv tmp.html tree.html
+
+    # Clearing unnecessary variables
+    sed -i "s| (MODEL)||g" tree.html
+    sed -i 's| class="a"||g' tree.html
+
+    # Extend table divider to include last column
+    sed -i 's|^\s\{,\}</td></tr>|        </td><td></td></tr>|g' tree.html
+
+    # Add SRL version to page title
+    sed -i "s|SRL_VER|$SRL_VER_CYCLE|g" tree.html
+    MODULE_HTML="<p class='title is-6'><font color=blue>Nokia SR Linux $SRL_VER_CYCLE YANG Browser</font></p>\n"
+    sed -i "s|MODULES|$MODULE_HTML|g" tree.html
+
+    # PATH SEARCH TOOL
+    #-------------------
+    # remove tools modules as they clash with regular leaves and are not relevant for the path search
+    cd $YANG_DIR_NAME
+    find ./ -name "*tools*.yang" -exec rm -f {} \;
+
+    # GENERATE PATHS. TEXT + JSON
+    docker run --rm -v $(pwd):/yang -w /yang $GNMIC_CONTAINER generate path --file $MODEL_PATH $GNMIC_ADDONS --types >$OUT_DIR/paths.txt
+    docker run --rm -v $(pwd):/yang -w /yang $GNMIC_CONTAINER generate path --file $MODEL_PATH $GNMIC_ADDONS --with-prefix --json >$OUT_DIR/paths.json
+
+    cd $SCRIPT_DIR
+    # copy per-release index page to output dir
+    if [ "$MODEL_TYPE" = "srl_nokia" ]; then
+      cp index.html.tmpl $OUT_DIR/index.html
+    elif [ "$MODEL_TYPE" = "openconfig" ]; then
+      cp oc-index.html.tmpl $OUT_DIR/index.html
+    fi
+    echo
+  fi
+done
+
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -51,7 +51,7 @@
     <div class="px-4 space-y-4 pt-[95px] lg:pt-[100px]">
       <ul class="space-y-2 text-gray-800 dark:text-gray-300">
         <li>
-          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/{release}{home ? '/tree' : ''}">
+          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/{release}{home ? '/tree' : ''}{model !== "nokia" ? "/?model=" + model : ""}">
             <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm9-10v.4A3.6 3.6 0 0 1 8.4 9H6.61A3.6 3.6 0 0 0 3 12.605M14.458 3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
             </svg>
@@ -59,7 +59,7 @@
           </a>
         </li>
         <li>
-          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/releases/{release}/tree.txt" target="_blank">
+          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/releases/{release}/{model !== "nokia" ? model + "/" : ""}tree.txt" target="_blank">
             <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.828 10h6.239m-6.239 4h6.239M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z"/>
             </svg>
@@ -67,7 +67,7 @@
           </a>
         </li>
         <li>
-          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/releases/{release}/paths.txt" target="_blank">
+          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/releases/{release}/{model !== "nokia" ? model + "/" : ""}paths.txt" target="_blank">
             <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.828 10h6.239m-6.239 4h6.239M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z"/>
             </svg>
@@ -75,15 +75,7 @@
           </a>
         </li>
         <li>
-          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="https://github.com/nokia/srlinux-yang-models/tree/{release}" target="_blank">
-            <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
-            </svg>
-            Source
-          </a>
-        </li>
-        <li>
-          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/releases/{release}/tree.html" target="_blank">
+          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="/releases/{release}/{model !== "nokia" ? model + "/" : ""}tree.html" target="_blank">
             <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm9-10v.4A3.6 3.6 0 0 1 8.4 9H6.61A3.6 3.6 0 0 0 3 12.605M14.458 3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
             </svg>
@@ -93,7 +85,7 @@
         {#if other?.length}
           {#each other as entry}
             <li>
-              <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="{entry.path}">
+              <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg whitespace" href="{entry.path}">
                 <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 1v4a1 1 0 0 1-1 1H1m5 8.514L4 12.5l2-2m4 4.014 2-2.014-2-2m5 7.5a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2v16Z"/>
                 </svg>
@@ -102,6 +94,14 @@
             </li>
           {/each}
         {/if}
+        <li>
+          <a data-sveltekit-reload class="flex items-center px-2 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 hover:rounded-lg" href="https://github.com/nokia/srlinux-yang-models/tree/{release}" target="_blank">
+            <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
+            </svg>
+            Source
+          </a>
+        </li>
       </ul>
     </div>
   </aside>
