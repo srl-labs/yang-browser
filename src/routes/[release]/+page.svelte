f494b8a6ddf0a28929e6dc849cfe99498f7a1d0f
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-05-01 20:01:27 +0200

    group hover

--- src/routes/[release]/+page.svelte
+++ src/routes/[release]/+page.svelte
@@ -253,10 +253,10 @@
             {#each $paginated as item}
               <tr class="bg-white dark:bg-gray-800 border-b dark:border-gray-700 text-gray-700 dark:text-gray-300">
                 <td class="px-3 py-1.5 font-fira text-[13px] tracking-tight">{getState(item)}</td>
-                <td class="px-3 py-1.5 font-fira text-[13px] tracking-tight">
-                  <div title="{item.description}" class="flex justify-between items-center">
+                <td class="px-3 py-1.5 font-fira text-[13px] tracking-tight group">
+                  <div class="flex justify-between items-center">
                     <span class="text-gray-900 dark:text-gray-300" use:highlight={[getSearchKeys($searchStore), getPath(item)]}></span>
-                    <button class="ml-1 p-0.5 rounded-lg text-gray-400 hover:bg-gray-300 dark:hover:bg-gray-600 hover:text-gray-900 dark:hover:text-white hover:cursor-pointer" use:copy={copyContent(item.path)} on:svelte-copy={copyEvent}>
+                    <button class="ml-1 p-0.5 rounded-lg text-gray-400 hover:bg-gray-300 dark:hover:bg-gray-600 hover:text-gray-900 dark:hover:text-white hover:cursor-pointer hidden group-hover:block" use:copy={copyContent(item.path)} on:svelte-copy={copyEvent}>
                       <svg class="clip w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                         <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M9 8v3a1 1 0 0 1-1 1H5m11 4h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-7a1 1 0 0 0-1 1v1m4 3v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-7.13a1 1 0 0 1 .24-.65L7.7 8.35A1 1 0 0 1 8.46 8H13a1 1 0 0 1 1 1Z"/>
                       </svg>
@@ -266,7 +266,7 @@
                     </button>
                   </div>
                 </td>
-                <td class="px-3 py-1.5 font-fira text-[13px] tracking-tight"><div title="{getEnumValues(item)}" use:highlight={[getSearchKeys($searchStore), item.type]}></div></td>
+                <td class="px-3 py-1.5 font-fira text-[13px] tracking-tight"><div use:highlight={[getSearchKeys($searchStore), item.type]}></div></td>
                 <td class="px-3 py-1.5 font-fira text-[13px] tracking-tight">
                   <div title="Show path in tree">
                     <a data-sveltekit-preload-data="tap" href="/{release}/tree?path={clearPathToTree(item.path)}{modelParam}">
