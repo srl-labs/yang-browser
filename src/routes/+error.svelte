4fedf886de92b5055c8d0da8cf5f67e883bdea4d
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-05-03 22:02:57 +0200

    home on error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,8 +6,9 @@
 			body: "flex h-screen p-7 font-nokia-headline-light",
 			container: "m-auto",
 			message: "rounded min-w-[400px]",
-			message_header: "text-white bg-red-400 px-5 py-3 rounded-t",
-			message_body: "bg-red-50 p-5 rounded-b text-red-500"
+			message_header: "text-white bg-red-400 px-5 py-3 rounded-t flex items-center justify-between",
+			message_body: "bg-red-50 p-5 rounded-b text-red-500",
+			back_home: "inline-flex items-center text-sm px-2 py-1 bg-red-600 hover:bg-red-700 rounded-lg"
 		}
 	}
 </script>
@@ -17,8 +18,16 @@
 		<article class="{custom.error.message}">
 			<div class="{custom.error.message_header}">
 				<p>Error {$page.status}</p>
+				<a href="../" class="{custom.error.back_home}">
+					<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+						<path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
+					</svg>
+					<span class="ml-0.5">Home</span>			
+				</a>
 			</div>
-			<div class="{custom.error.message_body}">{$page.error.message}</div>
+			<div class="{custom.error.message_body}">
+				<p>{$page.error.message}</p>
+			</div>
 		</article>
 	</div>
 </div>
