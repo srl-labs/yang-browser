93674e1498d0edd415509e70ff155b4566bdfedc
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-05-12 23:04:50 +0200

    popup header

--- src/lib/components/ComparePopup.svelte
+++ src/lib/components/ComparePopup.svelte
@@ -6,14 +6,12 @@
 <div class="flex h-screen p-7 font-nokia-headline-light">
   <div class="m-auto">
     <article class="rounded max-w-[550px]">
-			<div class="text-white bg-gray-500 px-5 py-3 rounded-t flex items-center justify-between">
-				<p>Nothing to Compare</p>
-				<a href="../" class="inline-flex items-center text-sm px-2 py-1 bg-gray-600 hover:bg-gray-700 rounded-lg">
-					<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-						<path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
-					</svg>
-					<span class="ml-0.5">Home</span>			
-				</a>
+			<div class="text-white bg-gray-500 px-5 py-3 rounded-t flex items-center">
+        <a href="../" class="flex mr-4"><img src="/images/navbar-logo.png" alt="Logo" width="25"/></a>
+				<div>
+          <p class="text-xs">SR Linux Yang Models</p>
+          <p>Nothing to Compare</p>
+        </div>
 			</div>
 			<div class="bg-gray-200 p-5 rounded-b">
         <p>No path or path types have changed between v{x} and v{y}. </p>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,14 +5,12 @@
 <div class="flex h-screen p-7 font-nokia-headline-light">
 	<div class="m-auto">
 		<article class="rounded md:min-w-[400px]">
-			<div class="text-white bg-red-400 px-5 py-3 rounded-t flex items-center justify-between">
-				<p>Error {$page.status}</p>
-				<a href="../" class="inline-flex items-center text-sm px-2 py-1 bg-red-600 hover:bg-red-700 rounded-lg">
-					<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-						<path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
-					</svg>
-					<span class="ml-0.5">Home</span>			
-				</a>
+			<div class="text-white bg-red-500 px-5 py-3 rounded-t flex items-center">
+        <a href="../" class="flex mr-4"><img src="/images/navbar-logo.png" alt="Logo" width="25"/></a>
+				<div>
+          <p class="text-xs">SR Linux Yang Models</p>
+          <p>Error {$page.status}</p>
+        </div>
 			</div>
 			<div class="bg-red-50 p-5 rounded-b text-red-500">
 				{#if $page.error}
