df53d91e9155cbe24aa060f31314a094e20988a3
Roman Dodin <dodin.roman@gmail.com>
2024-07-11 10:16:28 +0300

    persist json features per platform and use gnmic 0.38

--- refer/extract-features.sh
+++ refer/extract-features.sh
@@ -7,7 +7,7 @@
 # the script first creates a json file per each platform with the features extracted from the platform using `info from state system features` command
 # then it aggregates all the json files into a single file that is then processed by the yang browser
 
-# note, that some features reported by the platform might not be present in the YANG, which cause the pyang to error.
+# note, that some features for platforms prior to 24.3.1 release reported by the platform might not be present in the YANG, which cause the pyang to error.
 # to date the following features were identified as invalid for D2L platform, they have to be removed from the JSON file before `generate.sh` script is called
 # bgp-app-warm-restart
 # cam-cammgr-thread-programming
@@ -29,29 +29,38 @@
 # example:
 # SRL_VERSION=23.10.4 LIC_PATH=/opt/nokia/srl/23-10.lic bash refer/extract-features.sh
 # note, that the script doesn't remove the lab after the features are extracted.
+# the script must be executed from the root of this repo.
 
 set -e
 
 # export SRL_VERSION that is used by the containerlab to spin up the needed version of srl container
 export SRL_VERSION
 
+RELEASE_DIR=./static/releases/v${SRL_VERSION}
+FEATURES_DIR=${RELEASE_DIR}/platform_features
+
+# move the features file to the release folder
+echo "making directory ${FEATURES_DIR}"
+mkdir -p ${FEATURES_DIR}
+
 sudo -E containerlab deploy -c -t ./refer/all.clab.yml
 
 # remove all json files that might be there from previous extraction
-rm -f ./tmp/*.json
+rm -f ${FEATURES_DIR}/*.json
 
 for node in "7220-IXR-D1" "7220-IXR-D2" "7220-IXR-D2L" "7220-IXR-D3" "7220-IXR-D3L" "7220-IXR-D4" "7220-IXR-D5" "7220-IXR-H2" "7220-IXR-H3" "7220-IXR-H4" "7250-IXR-6" "7250-IXR-6e" "7250-IXR-10" "7250-IXR-10e"
 do
-    docker exec clab-all-${node} sr_cli 'info from state system features | as json' | jq .system.features >> ./tmp/${node}.json &
+    docker exec clab-all-${node} sr_cli 'info from state system features | as json' | jq .system.features >> ${FEATURES_DIR}/${node}.json &
 done
 
 wait
 echo "Features have been extracted to JSON files, proceeding with aggregating them into a single file..."
 
-# change to ./tmp
-pushd ./tmp
+# change to platform features directory
+pushd ${FEATURES_DIR}
 # file name to extract features for all platforms
-fn="features.txt"
+# it resides in the release dir, and not features dir
+fn="../features.txt"
 
 # Remove the output file if it already exists
 if [ -f $fn ]; then
@@ -69,11 +78,5 @@ for entry in *.json; do
     echo "$platform: $features" >> $fn
 done
 popd
-
-# move the features file to the release folder
-echo "making directory ./static/releases/v$SRL_VERSION"
-mkdir -p ./static/releases/v$SRL_VERSION
-
-mv ./tmp/$fn ./static/releases/v$SRL_VERSION
 
 echo "Features aggregated into a features.txt file!"
