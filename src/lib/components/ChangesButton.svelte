5c1a714cee9086281604ffa4ce6c11368aa7f7b5
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-09-27 17:28:47 +0200

    address #55 #59

--- src/lib/components/ChangesButton.svelte
+++ src/lib/components/ChangesButton.svelte
@@ -9,7 +9,7 @@
     bg-gray-100 hover:bg-gray-200 dark:bg-gray-700 dark:hover:bg-gray-600 
     border border-gray-200 dark:border-gray-600">
     {#if compareInput === ""}
-      Changes
+      All Changes
       <svg class="w-2.5 h-2.5 ms-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
       </svg>
