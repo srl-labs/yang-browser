0dc9ad4af5e9189d22f39d3b7c7b23d80fae76f3
Siva Sivakumar <siva.sivakumar@nokia.com>
2024-10-23 23:48:35 +0200

    missing $ for pagination

--- src/routes/[release]/Pagination.svelte
+++ src/routes/[release]/Pagination.svelte
@@ -20,8 +20,8 @@
       </svg>
     </button>
     <button class="ml-2 text-white rounded 
-        {end == total ? 'bg-gray-300 dark:bg-gray-500 opacity-50 cursor-not-allowed' : 'bg-gray-400 hover:bg-gray-600'}" 
-        disabled="{end == total}" on:click={() => updateTable($end)}>
+        {$end == $total ? 'bg-gray-300 dark:bg-gray-500 opacity-50 cursor-not-allowed' : 'bg-gray-400 hover:bg-gray-600'}" 
+        disabled="{$end == $total}" on:click={() => updateTable($end)}>
       <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd"/>
       </svg>
